ecb41c88c0c76b967b368c08f11480ca1907025d
Wanbok Choi <choi@wanbok.com>
2017-03-04 14:31:28 +0900

    Add Model, StyledString

---
+++ SmartMarkupViewer/Models/Model.swift
@@ -0,0 +1,35 @@
+//
+//  Model.swift
+//  
+//
+//  Created by 1024jp on 2017/03/04.
+//
+//
+
+struct Document {
+    
+    let rawString: String
+    let language: Language
+    
+    let sections: [Section]
+    
+}
+
+struct Section {
+    
+    let title: StyledString?
+    let level: Int
+    let contents: [Block]
+    let subsections: [Section]
+}
+
+
+enum Block {
+    
+    case paragraph(attributedString: StyledString)
+    case list(list: [StyledString])
+    case code(language: String?, content: String)
+}
+
+struct Language {}
+
